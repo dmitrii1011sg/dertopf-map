1a837ce679fc695eeb370eeef7a20b55d4e69977
Gorshkov Dmitrii <dmitrii1011sg@gmail.com>
2026-04-12 00:15:39 +0300

    feat: dt confirm dialog

--- apps/map-app/src/app/components/entity-list/entity-list.component.ts
+++ apps/map-app/src/app/components/entity-list/entity-list.component.ts
@@ -1,12 +1,21 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, DestroyRef, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { toSignal } from '@angular/core/rxjs-interop';
-import { map } from 'rxjs';
-import { ENTITY_TYPES, EntityType, MapActions, mapFeature } from '@data-access';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { filter, map } from 'rxjs';
+import {
+  ENTITY_TYPES,
+  EntityType,
+  MapActions,
+  MapEntity,
+  mapFeature,
+  MapPoint,
+  MapPolygon,
+  MapPolyline,
+} from '@data-access';
 import { CommonModule } from '@angular/common';
 import { DtSidebarLayoutSectionsModule } from '../../layouts/sidebar-layout';
-import { DtPageTitleComponent } from '@dertopf-ui';
+import { DtConfirmDialogComponent, DtPageTitleComponent } from '@dertopf-ui';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,6 +27,7 @@ import {
   faTrash,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Dialog } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-entity-list',
@@ -36,8 +46,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   styleUrls: ['./entity-list.component.scss'],
 })
 export class EntityListComponent {
-  private route = inject(ActivatedRoute);
-  private store = inject(Store);
+  private readonly route = inject(ActivatedRoute);
+  private readonly store = inject(Store);
+  private readonly dialog = inject(Dialog);
+  private readonly destroyRef = inject(DestroyRef);
 
   readonly currentType = toSignal<EntityType>(
     this.route.paramMap.pipe(
@@ -67,23 +79,21 @@ export class EntityListComponent {
 
   readonly currentEntities = computed(() => {
     const type = this.currentType();
-    let state: any;
+    const selectAll = <T>(state: {
+      ids: string[] | number[];
+      entities: Record<string, T>;
+    }): T[] => state?.ids?.map((id) => state.entities[id]) || [];
 
     switch (type) {
       case 'point':
-        state = this.points();
-        break;
+        return selectAll(this.points()) as MapPoint[];
       case 'polyline':
-        state = this.polylines();
-        break;
+        return selectAll(this.polylines()) as MapPolyline[];
       case 'polygon':
-        state = this.polygons();
-        break;
+        return selectAll(this.polygons()) as MapPolygon[];
       default:
         return [];
     }
-
-    return state?.ids?.map((id: string) => state.entities[id]) || [];
   });
 
   onToggleVisibility(id: string, event: Event): void {
@@ -99,9 +109,24 @@ export class EntityListComponent {
     }
   }
 
+  confirmDelete(entity: MapEntity): void {
+    const dialogRef = this.dialog.open<boolean>(DtConfirmDialogComponent, {
+      data: {
+        title: 'Delete',
+        message: `Are you sure you want to delete ${entity.name || 'this object'}?`,
+      },
+    });
+
+    dialogRef.closed
+      .pipe(filter(Boolean), takeUntilDestroyed(this.destroyRef))
+      .subscribe(() => {
+        this.onDelete(entity.id);
+      });
+  }
+
   onDelete(id: string): void {
     const type = this.currentType();
-    if (confirm('Delete this entity?') && type) {
+    if (type) {
       this.store.dispatch(
         MapActions.deleteEntity({
           id,
