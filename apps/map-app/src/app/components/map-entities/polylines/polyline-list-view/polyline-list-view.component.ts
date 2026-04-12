e595a28dff461a8342198d34940d2ddd8cb8650a
Gorshkov Dmitrii <dmitrii1011sg@gmail.com>
2026-04-12 23:57:51 +0300

    fix: hotfix polyline showhide

--- apps/map-app/src/app/components/map-entities/polylines/polyline-list-view/polyline-list-view.component.ts
+++ apps/map-app/src/app/components/map-entities/polylines/polyline-list-view/polyline-list-view.component.ts
@@ -80,7 +80,7 @@ export class PolylineListComponent {
     this.store.dispatch(
       MapActions.toggleEntityVisibility({
         id: entity.id,
-        typeEntity: 'polygon',
+        typeEntity: 'polyline',
       }),
     );
   }
